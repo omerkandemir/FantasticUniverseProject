14179ddb4e94c532660d1ed595d46ce8ede13f25
Ömer Canberk Kandemir <omer1805.mk@gmail.com>
2024-08-28 20:46:40 +0300

    Add transaction aspect to handle atomic operations

---
+++ Backend/NLayer/Core/Aspect/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using NLayer.Core.Utilities.Interceptors;
+using System.Transactions;
+
+namespace NLayer.Core.Aspect.Autofac.Transaction;
+
+[Serializable]
+public class TransactionScopeAspect : MethodInterception
+{
+
+    public override void Intercept(IInvocation invocation)
+    {
+        using (var transactionScope = new TransactionScope(TransactionScopeOption.Required,
+                        new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted })) //IsolationLevel veritabanı işlemlerinin diğer işlemlerden nasıl izole edileceğini belirler. ReadCommitted, bir işlem sırasında okunan verilerin, başka bir işlem tarafından henüz tamamlanmamış değişikliklerini engeller.
+        {
+            try
+            {
+                invocation.Proceed();
+                transactionScope.Complete();
+            }
+            catch (Exception e)
+            {
+                transactionScope.Dispose();
+                OnException(invocation, e);
+                throw;
+            }
+        }
+    }
+}
